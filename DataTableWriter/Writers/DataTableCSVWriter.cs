cb0260b754840fde71126d0dd52b52e1c2fadc64
Daniel Rahm <drahm@tableau.com>
2016-08-03 19:03:29 -0700

    Data compression method is complete. Need to move to correct class before pushing.

--- DataTableWriter/Writers/DataTableCSVWriter.cs
+++ DataTableWriter/Writers/DataTableCSVWriter.cs
@@ -3,7 +3,9 @@ using log4net;
 using System;
 using System.Data;
 using System.IO;
+using System.Linq;
 using System.Reflection;
+using System.Text;
 
 namespace DataTableWriter.Writers
 {
@@ -45,7 +47,8 @@ namespace DataTableWriter.Writers
             }
 
             Log.Debug(String.Format("Writing {0} {1} to CSV..", table.Rows.Count, "record".Pluralize(table.Rows.Count)));
-            foreach (DataRow row in table.Rows)
+            var compressedTable = compressTable(table);
+            foreach (DataRow row in compressedTable.Rows)
             {
                 WriteRow(row);
             }
@@ -94,6 +97,141 @@ namespace DataTableWriter.Writers
             {
                 Log.Error("Error writing record to CSV: " + ex.Message);
             }
+        }
+
+        protected static string BuildSelect(string category1, string category2, string category3, string category4, string category5, string category6)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (category1 != "")
+            {
+                sb.AppendFormat("Cluster = '{0}'", category1);
+            }
+            else
+            {
+                sb.Append("Cluster Is Null");
+            }
+
+            if (category2 != "")
+            {
+                sb.AppendFormat(" And Machine = '{0}'", category2);
+            }
+            else
+            {
+                sb.Append(" And Machine Is Null");
+            }
+
+            if (category3 != "")
+            {
+                sb.AppendFormat(" And Source = '{0}'", category3);
+            }
+            else
+            {
+                sb.Append(" And Source Is Null");
+            }
+
+            if (category4 != "")
+            {
+                sb.AppendFormat(" And Category = '{0}'", category4);
+            }
+            else
+            {
+                sb.Append(" And Category Is Null");
+            }
+
+            if (category5 != "")
+            {
+                sb.AppendFormat(" And Instance = '{0}'", category5);
+            }
+            else
+            {
+                sb.Append(" And Instance Is Null");
+            }
+
+            if (category6 != "")
+            {
+                sb.AppendFormat(" And Unit = '{0}'", category6);
+            }
+            else
+            {
+                sb.Append(" And Unit Is Null");
+            }
+
+            return sb.ToString();
+        }
+
+        protected static DataTable compressTable(DataTable table)
+        {
+            Log.Info("Printing original table");
+            foreach (DataRow row in table.Rows)
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (var thing in row.ItemArray)
+                {
+                    sb.Append(thing.ToString() + "|");
+                }
+                Log.Info(sb);
+            }
+
+            var distinctValues = (from DataRow dr in table.Rows
+                                  orderby dr["Instance"].ToString()
+                                  select new
+                                  {
+                                      temp1 = dr["Cluster"].ToString(),
+                                      temp2 = dr["Machine"].ToString(),
+                                      temp3 = dr["Source"].ToString(),
+                                      temp4 = dr["Category"].ToString(),
+                                      temp5 = dr["Instance"].ToString(),
+                                      temp6 = dr["Unit"].ToString()
+                                  }).Distinct();
+
+            Log.Info("Printing unique values");
+            foreach (var a in distinctValues)
+            {
+                Log.Info(a.ToString());
+            }
+             
+            var newTable = table.Clone();
+
+            foreach (var item in distinctValues)
+            {
+                DataRow[] foundRows;
+                var selectString = BuildSelect(item.temp1, item.temp2, item.temp3, item.temp4, item.temp5, item.temp6);
+                foundRows = table.Select(selectString);
+
+                DataRow newRow = null;
+                foreach (var singleRow in foundRows)
+                {
+                    if (newRow == null)
+                    {
+                        newRow = singleRow;
+                    }
+                    else
+                    {
+                        foreach (DataColumn column in newRow.Table.Columns)
+                        {
+                            var columnValue = newRow[column.ColumnName].ToString();
+                            var comparedValue = singleRow[column.ColumnName].ToString();
+                            if (String.IsNullOrWhiteSpace(columnValue) && !String.IsNullOrWhiteSpace(comparedValue))
+                            {
+                                newRow[column.ToString()] = singleRow[column.ToString()];
+                            }
+                        }
+                    }
+                }
+                newTable.ImportRow(newRow);
+            }
+
+            Log.Info("Printing compressed table");
+            foreach (DataRow row in newTable.Rows)
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (var thing in row.ItemArray)
+                {
+                    sb.Append(thing.ToString() + "|");
+                }
+                Log.Info(sb);
+            }
+            return newTable;
         }
 
         #endregion Protected Methods
